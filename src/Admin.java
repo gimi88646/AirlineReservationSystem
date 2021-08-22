5147e2765802f674ffe95765173511846e1973a3
AliAhmedKhan <alistpaulscr7@gmail.com>
2021-08-23 02:02:47 +0500

    Ignore Changes

--- src/Admin.java
+++ src/Admin.java
@@ -6,7 +6,9 @@ public class Admin extends Person {
 
     // implementation of methods that are displayed in driver class
     //this method takes to-from time.. number of passengers for a carrier as parameters
-    public void addRoute(){}
+    public void addRoute(){
+        
+    }
     public void cancelRoute(){}
     //some more methods
 }
