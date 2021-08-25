fa4cb93aa5842aa4dbc89c906046a47b3392152b
AliAhmedKhan <alistpaulscr7@gmail.com>
2021-08-25 21:37:23 +0500

    Admin.Java changed slightly

--- src/Admin.java
+++ src/Admin.java
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-import java.util.ArrayList;
-=======
 import javax.xml.transform.Result;
 import java.awt.image.RescaleOp;
 import java.sql.ResultSet;
 import java.sql.SQLException;
->>>>>>> 0cd97e9cc82812130aa5217d60c5daa4786a028d
 
 public class Admin extends Person {
 
@@ -16,27 +12,6 @@ public class Admin extends Person {
 
     // implementation of methods that are displayed in driver class
     //this method takes to-from time.. number of passengers for a carrier as parameters
-<<<<<<< HEAD
-    public void addRoute(ArrayList<String> flightinfo){
-        for (int i=0; i<=flightinfo.size(); i++){
-            String flight = flightinfo.get(i);
-            
-        }
-    }
-    public void cancelRoute(){}
-    //some more methods
-    /*
-    * flight ke database ke andar ek column he inactiveSince ka column hoga, jsme date us waqt aegi jab admin us particular flghtId ko inactive krdega
-    * ab ye flight srf us din ki flights complete karegi, jis dino ki booking sme initialize ho chuki he
-    * or koi naye din ki booking nahi legi
-    * jese hii tamam flights complete karegii.. phr databse se dlete hojaegi
-    * active till kaa column ...
-    * select bookedForDate from bookings whereflightId  ORDER By bookedforDate DESC LIMIT 1
-    * no more bookings on new date.. that is if any date has at least 1 booking it can take passengers
-    *
-    * js flight ko inactive kiya
-    * */
-=======
     public void addRoute() throws SQLException {
         statement.execute("INSERT INTO Flights(flightId,travelTo,travelFrom) VALUES('120','Lahore','Peshawar');");
     }
@@ -67,5 +42,4 @@ public class Admin extends Person {
         }
         return mostRecentlyBookedDate;
     }
->>>>>>> 0cd97e9cc82812130aa5217d60c5daa4786a028d
 }
